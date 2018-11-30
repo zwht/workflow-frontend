612194707a4c709bf44c108d51d285dac4aa5611
魏翼超然 <1512763623@qq.com>
2018-11-30 11:51:41 +0800

    集成ng-alain

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, TestModuleMetadata } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { setUpTestBed } from '@testing/common.spec';
+
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+describe('Component: App', () => {
+  setUpTestBed(<TestModuleMetadata>{
+    declarations: [AppComponent],
+    providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'doorPieceworkFrontEndNew'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('doorPieceworkFrontEndNew');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to doorPieceworkFrontEndNew!');
+    const comp = fixture.debugElement.componentInstance;
+    expect(comp).toBeTruthy();
   });
 });
