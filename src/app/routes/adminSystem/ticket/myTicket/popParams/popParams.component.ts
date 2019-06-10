4fcc121f52ffef72d32bf3a5f5baa1084725c048
wei zhao <weizhao@weideMacBook.local>
2019-06-10 17:07:26 +0800

    bug

--- src/app/routes/adminSystem/ticket/myTicket/popParams/popParams.component.ts
+++ src/app/routes/adminSystem/ticket/myTicket/popParams/popParams.component.ts
@@ -15,46 +15,44 @@ export class PopParamsComponent implements OnInit {
   title;
 
   @Input() gxList: any;
-  list = []
+  list = [];
   constructor(
     private http: _HttpClient,
     private modal: NzModalRef,
     public activatedRoute: ActivatedRoute,
-  ) {
-
-  }
+  ) {}
 
   ngOnInit() {
     this.list = JSON.parse(this.gxList.gxParams).map(item => {
       if (item.type == 2) {
         item.params = item.params.map(obj => {
-          const l = obj.split('#')
+          const l = obj.split('#');
           return {
             old: obj,
             name: l[0],
-            value: l[1]
-          }
-        })
-        item.value = item.params[0].value
+            value: l[1],
+          };
+        });
+        item.value = item.params[0].value;
       }
       if (item.type == 3) {
-        item.value = item.params
+        item.value = item.params;
       }
-      return item
-    })
+      return item;
+    });
   }
-  save(door) {
+  save() {
     const gxIds = this.gxList.gxIds.split(','),
       gxValues = this.gxList.gxValues.split(',');
     const gxList = [];
     this.list.forEach(item => {
       gxIds.forEach((id, i) => {
         if (id === item.gxId) {
-          gxValues[i] = item.value+''
+          gxValues[i] = item.value + '';
         }
       });
     });
-    this.gxList.gxValues=gxValues.join(',')
+    this.gxList.gxValues = gxValues.join(',');
     this.modal.destroy({
       data: this.gxList,
     });
